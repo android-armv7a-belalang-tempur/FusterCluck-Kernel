87e86893b816851bb15d0be48840bcf76a9b9b9d
Dorimanx <yuri@bynet.co.il>
2014-10-22 17:19:27 +0300

    FORCE FAST CHARGE: Delete useless FAKE AC hack. it's brake ROM LED.
    This has no effect on battery charge. it's just say AC and not USB
    But brake charge LED after unplug, it's still turned ON.
    and charge mode is not disabled, so wakelock is held, user loose
    power, as CPU dont sleep.
    
    Time for this HACK to be cleaned. it's was OFF by default.
    but users used it, and posted nags. purge is the best option here.
    
    Conflicts:
    	drivers/usb/dwc3/dwc3_otg.c

--- drivers/usb/dwc3/dwc3_otg.c
+++ drivers/usb/dwc3/dwc3_otg.c
@@ -951,15 +951,7 @@ static void dwc3_otg_sm_work(struct work_struct *w)
 	case OTG_STATE_UNDEFINED:
 		dwc3_otg_init_sm(dotg);
 		if (!dotg->psy) {
-#ifdef CONFIG_FORCE_FAST_CHARGE
-			if ((force_fast_charge > 0) &&
-					(fake_charge_ac == FAKE_CHARGE_AC_ENABLE))
-				dotg->psy = power_supply_get_by_name("ac");
-			else
-				dotg->psy = power_supply_get_by_name("usb");
-#else
 			dotg->psy = power_supply_get_by_name("usb");
-#endif
 
 			if (!dotg->psy)
 				dev_err(phy->dev,
